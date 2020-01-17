d037e36515b3efe76d364df03524f59a8eb29b05
krumilurfigur <gabrieloberg3@gmail.com>
2020-01-17 11:14:54 +0100

    2020-01-17

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ Singleplayer eller 2 player,
 
 2020-01-16
 Vi har löst 1player/2player valet, imorgon ska vi börja på random word generatorn
+
+2020-01-17
+Vi har börjat med att läsa in ord från en fil för att använda i random word generatorn 
--- src/RWG.java
+++ src/RWG.java
@@ -5,10 +5,17 @@ Gabriel Öberg
  */
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class RWG {
     public static void main(String[] args) {
-        Scanner kb = new Scanner(System.in);
+        Scanner scan = new Scanner("words.txt");
+        String[] words = new String[3847];
 
+        for (int i = 0; i < words.length; i++) {
+            words[i] = scan.nextLine();
+            System.out.println(words[i]);
+        }
     }
 }
